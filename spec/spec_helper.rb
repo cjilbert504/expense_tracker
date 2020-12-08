d4121d40afbdaf5d9f6030895dd69d5e1995d9f7
Collin Jilbert <cjilbert504@gmail.com>
2020-12-08 13:22:15 -0600

    Configure default rspec format to doc and enable running specs in random order to surface order dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,9 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
+  config.default_formatter = "doc"
+  config.order = :random
+  
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
@@ -78,7 +80,6 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = "doc"
   end
 
   # Print the 10 slowest examples and example groups at the
@@ -90,7 +91,6 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
