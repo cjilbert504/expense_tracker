c5158e4ef1f44665583c3c4aff9aa30e67bcbe00
Collin Jilbert <cjilbert504@gmail.com>
2020-12-11 19:14:14 -0600

    Add specs for unhappy path and begin building out the ledger class

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -2,8 +2,6 @@ require 'rack/test'
 require_relative '../../../app/api'
 
 module ExpenseTracker
-	RecordResult = Struct.new(:success?, :expense_id, :error_message)
-
 	RSpec.describe API do
 		include Rack::Test::Methods
 
@@ -38,8 +36,25 @@ module ExpenseTracker
 			end
 
 			context 'when the expense fails validation' do
-				it 'returns an error message'
-				it 'responds with a 422 (Unprocessable entity)'
+				let(:expense) { { 'some' => 'data' } }
+
+				before do
+					allow(ledger).to receive(:record)
+						.with(expense)
+						.and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+				end
+
+				it 'returns an error message' do
+					post '/expenses', JSON.generate(expense)
+
+					parsed = JSON.parse(last_response.body)
+					expect(parsed).to include('error' => 'Expense incomplete')
+				end
+
+				it 'responds with a 422 (Unprocessable entity)' do
+					post '/expenses', JSON.generate(expense)
+					expect(last_response.status).to eq(422)
+				end
 			end
 		end
 	end
